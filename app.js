15de49178458380617780a958c2a709c33ac4f6d
Risa Tsunematsu <purachan1818@gmail.com>
2023-05-25 13:34:45 +0000

    this

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 //"box1タグ内のcssを、 'background-color': '#0000FF','height': '100px'で書き換えなさい"という意味
-$(function (){
-  $('.box1').slideDown(function (){
-   $('.box1').css({
-     'background-color': '#0000FF',
-     'height': '200px',
-     'width': '100px'
-   }).slideUp();
+
+
+$(function(){
+  $('.box1').on('click', function(){
+    $(this).slideUp();
   });
 });
