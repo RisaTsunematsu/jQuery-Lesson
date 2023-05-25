aed0c73fa1dba80e03ea7da472cef0aab58678dd
Risa Tsunematsu <purachan1818@gmail.com>
2023-05-25 13:37:57 +0000

    children

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 //"box1タグ内のcssを、 'background-color': '#0000FF','height': '100px'で書き換えなさい"という意味
-
-
 $(function(){
-  $('.box1').on('click', function(){
-    $(this).slideUp();
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
   });
 });
